d96cbe5a7830d2b2c1db9cb81ce728d3b208d412
irfanariyaz <irfana.27@gmail.com>
2024-03-25 13:49:07 -0700

    eevert "added charts"
    This reverts commit 97f8adff5cc01eb04f97a0ece602d27c61763a44.

--- src/main/java/com/example/Stocker/Repository/AnnualReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Stocker.Repository;
-
-import com.example.Stocker.model.AnnualReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnnualReportRepository extends JpaRepository<AnnualReport,Long> {
-}
